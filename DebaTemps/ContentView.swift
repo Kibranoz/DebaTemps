8fddd889a56c39ec76a797912eb4fd581cb61787
Louis <louisrcouture@protonmail.com>
2019-12-22 01:39:40 -0500

    Initial Commit

---
+++ DebaTemps/ContentView.swift
@@ -0,0 +1,104 @@
+//
+//  ContentView.swift
+//  DebaTemps
+//
+//  Created by Louis Couture on 2019-11-22.
+//  Copyright © 2019 lrcstudios. All rights reserved.
+//
+
+import SwiftUI
+ 
+
+struct ContentView: View {
+    @State var a = "Chronometre"
+    @State var partie = "Partie du débat"
+    @State var tempsString = "Temps ici"
+    @State var enCours = "CanadienParlementaire - Commencer"
+    @State var pausePlay = "⏸"
+    @State var tempsMillieu = 420;
+    //il va falloir un bouton pause
+    var body: some View {
+        VStack {
+            Text(a)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(Color.black)
+                .lineLimit(nil)
+            
+            Text(partie)
+                .font(.subheadline)
+                
+            Button(action: {
+                self.enCours = "Recommencer"
+                var round = 7
+                let chronoMillieu = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (chronoMilieu) in
+                    if round > 2 {
+                    if self.tempsMillieu != 0{
+                        self.tempsString = String(formatTime(time: self.tempsMillieu))
+                        if self.pausePlay == "⏸"{
+                            self.tempsMillieu -= 1
+                        }
+                        if self.tempsMillieu > 360{
+                            self.partie = "1 min temps protégé"
+                        }
+                        if self.tempsMillieu < 360 && self.tempsMillieu > 60 {
+                            self.partie = "5 min temps non protégé"
+                        }
+                        if self.tempsMillieu < 60 {
+                            self.partie = "1 min temps protégé"
+                        }
+                    }
+                    else{
+                        //chronoMilieu.invalidate()
+                        round -= 1
+                        self.tempsMillieu = 420
+                        
+                        //self.partie = "Fin"
+                    }
+                }
+                    else{
+                        chronoMilieu.invalidate()
+                        self.partie = "Fin"
+                    }
+                    
+                }
+                self.a = "CP"
+                }) {
+                    Text(enCours)
+            }
+            Text(tempsString)
+            HStack{
+                Button(action: {
+                    switch (self.pausePlay) {
+                    case "▶️" :
+                        self.pausePlay = "⏸"
+                        break
+                    case "⏸" :
+                        self.pausePlay = "▶️"
+                        break
+                    default:
+                        self.pausePlay = "Erreur"
+                    }
+                    
+                    
+                }) {
+                    Text(pausePlay)
+                        .lineLimit(nil)
+                    
+                }
+                Button(action: {
+                    prochainTour(time: &self.tempsMillieu)
+                }, label: {
+                    Text("⏩")
+                } )
+            }
+        }
+
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DebaTemps/logic.swift
@@ -0,0 +1,20 @@
+//
+//  logic.swift
+//  DebaTemps
+//
+//  Created by Louis Couture on 2019-11-22.
+//  Copyright © 2019 lrcstudios. All rights reserved.
+//
+
+import Foundation
+
+
+func formatTime(time:Int) -> String {
+    let minutes:Int = time/60
+    let seconds:Int = time%60
+    return String(minutes) + ":" + String(seconds)
+}
+func prochainTour(time: inout Int)->Void{
+    let tempsAvantLaFin = time % 420
+    time -= tempsAvantLaFin
+}
